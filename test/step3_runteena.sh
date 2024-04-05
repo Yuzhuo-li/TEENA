c8104e12008626a15d924ea1e45dca2c9964892c
Yuzhuo-li <141910868+Yuzhuo-li@users.noreply.github.com>
2024-04-05 21:18:28 +0800

    Update step3_runteena.sh

--- test/step3_runteena.sh
+++ test/step3_runteena.sh
@@ -1,1 +1,1 @@
-python teena.py -q GATA3_hg38.bed -d hg38.repbase.bed -fa hg38.fa -ch hg38.chrom.sizes -a Homo_sapiens.GRCh38.110.gtf -o test
+python ../teena.py -q GATA3_hg38.bed -d hg38.repbase.bed -fa hg38.fa -ch hg38.chrom.sizes -a Homo_sapiens.GRCh38.110.gtf -o test
