4f857f3ec4f6716980c2fcb064a640d96c6d2b7f
Yuzhuo-li <141910868+Yuzhuo-li@users.noreply.github.com>
2024-04-06 19:21:48 +0800

    Update step3_runteena.sh

--- test/step3_runteena.sh
+++ test/step3_runteena.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+# This is the code to run teena with default parameters.
 python ../teena.py -q GATA3_hg38.bed -d hg38.repbase.bed -fa hg38.fa -ch hg38.chrom.sizes -a Homo_sapiens.GRCh38.110.gtf -o test
