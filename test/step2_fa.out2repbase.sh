a7a20d58c1bd6407c188cf90336160e5e3749bf9
Yuzhuo-li <141910868+Yuzhuo-li@users.noreply.github.com>
2024-04-20 14:58:22 +0800

    Update step2_fa.out2repbase.sh

--- test/step2_fa.out2repbase.sh
+++ test/step2_fa.out2repbase.sh
@@ -2,18 +2,10 @@
 # The prerequisite for the successful execution of this command is the installation of bedops.
 gunzip *.gz && mv ./hg38.fa.out hg38.repbase.txt;
 
-for i in *.repbase.txt; do
-    j=${i/.txt/}
-    if [ -e ${j}.bed ]; then
-        echo "Skip: $i"
-    else
-        echo "Processing $i ..."
- # To prevent the existence of bugs, it is recommended to use anabsolute path of convert2bed (for example: /home/download/bedops/bin/convert2bed)       
-        convert2bed <$i >${j}.full.bed
-        cat ${j}.full.bed | awk '{print $1 "\t" $2 "\t" $3 "\t" $11 ":" $4 "\t"
-$5 "\t" $6}' >${j}.bed
-    fi
-done
+# To prevent the existence of bugs, it is recommended to use anabsolute path of rmsk2bed (for example: /home/download/bedops/bin/rmsk2bed)       
+rmsk2bed <hg38.repbase.txt >hg38.repbase.full.bed
+cat hg38.repbase.full.bed | awk '{print $1 "\t" $2 "\t" $3 "\t" $11 ":" $4 "\t" $5 "\t" $6}' >hg38.repbase.bed
+   
 
 rm -rf ./hg38.repbase.full.bed; 
 rm -rf ./hg38.repbase.txt
