af8c9349a5443fb708fc422bf22c46bb65099c06
Yuzhuo-li <141910868+Yuzhuo-li@users.noreply.github.com>
2024-04-21 13:03:40 +0800

    Update step3_runteena.sh

--- test/step3_runteena.sh
+++ test/step3_runteena.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # This is the code to run teena with default parameters.
+# It is recommended to have a version of Python above 3.8 to facilitate drawing.
 python ../teena.py -q GATA3_hg38.bed -d hg38.repbase.bed -fa hg38.fa -ch hg38.chrom.sizes -a Homo_sapiens.GRCh38.110.gtf -o test
