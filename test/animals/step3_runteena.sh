fb65c4c8ea537e2a154a72172e447fce23521bb7
Yuzhuo-li <141910868+Yuzhuo-li@users.noreply.github.com>
2024-04-23 21:51:34 +0800

    Update step3_runteena.sh

--- test/animals/step3_runteena.sh
+++ test/animals/step3_runteena.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # This is the code to run teena with default parameters.
 # It is recommended to have a version of Python above 3.8 to facilitate drawing.
-python ../teena.py -q GATA3_hg38.bed -d hg38.repbase.bed -fa hg38.fa -ch hg38.chrom.sizes -a Homo_sapiens.GRCh38.110.gtf -o test
+# GATA3_hg38.bed is only a test file for code running, it is recommended to use a bed file that matches the species genome for analysis.
+python ../../teena.py -q GATA3_hg38.bed -d hg38.repbase.bed -fa hg38.fa -ch hg38.chrom.sizes -a Homo_sapiens.GRCh38.110.gtf -o test
