efb6982fdb0728daf2e05a63f689cf146169cc7e
Yuzhuo-li <141910868+Yuzhuo-li@users.noreply.github.com>
2024-04-23 21:50:58 +0800

    Update step5_runteena.sh

--- test/plants/step5_runteena.sh
+++ test/plants/step5_runteena.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # This is the code to run teena with default parameters.
 # It is recommended to have a version of Python above 3.8 to facilitate drawing.
+# GATA3_hg38.bed is only a test file for code running, it is recommended to use a bed file that matches the species genome for analysis.
 python ../../teena.py -q GATA3_hg38.bed -d irgsp1.repbase.bed -fa Oryza_sativa.IRGSP-1.0.dna.toplevel.fa -ch irgsp1.chrom.sizes -a Oryza_sativa.IRGSP-1.0.58.gtf -o rice
