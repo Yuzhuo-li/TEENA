c3517dd50a766bce4bbe3d224be94dd688156317
Yuzhuo-li <141910868+Yuzhuo-li@users.noreply.github.com>
2024-04-23 19:09:51 +0800

    Update modify_bed_column.sh

--- test/plants/modify_bed_column.sh
+++ test/plants/modify_bed_column.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-
-# 检查输入参数
+# This is a script to process TE annotation of plants
+# Check input bed file
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <bed_file>"
     exit 1
 fi
 
-# 输入的BED文件
 bed_file=$1
 
-# 检查文件是否存在
 if [ ! -f "$bed_file" ]; then
     echo "Error: File '$bed_file' not found!"
     exit 1
 fi
 
-# 创建一个新的输出文件名
+# Create a new name of output bed file
 output_file="${bed_file%.bed}.modified.bed"
 
-# 处理BED文件
+# process the bed file
 awk 'BEGIN {OFS="\t"} {
     if (split($4, arr, ":") == 1 && split($4, arr, "/") == 2) {
-        $4 = $4 ":NA"  # 如果第四列有两个部分通过 / 分隔，但没有 :，添加 :NA
+        $4 = $4 ":NA"  # If there are two strings separated by "/" in the fourth column, but do not have ":", add ":NA"
     }
     print $0
 }' "$bed_file" > "$output_file"
 
 echo "Modified BED file created: $output_file"
-
