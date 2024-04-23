60f66dfedfbecee813d26f3ec96c4416f835e9ef
Yuzhuo-li <141910868+Yuzhuo-li@users.noreply.github.com>
2024-04-23 19:42:49 +0800

    Update step1_datadownload.sh

--- test/animals/step1_datadownload.sh
+++ test/animals/step1_datadownload.sh
@@ -1,5 +1,5 @@
 # Download the genome sequence files, genome length files, TE annotation files, and genome annotation files required to run teena.
-# download: from UCSC and Ensembl.
+# Download: from UCSC and Ensembl.
 # This is an example for data download of homo_sapiens(hg38). 
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
